c1e55f6696544cc2491ede118c8d3d20ac62a4c5
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-04 20:26:20 +0900

    詳細ページが誰にでも見られるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'prototypes#index'
   resources :users
-  resources :prototypes do
-    resources :comments,only: :create
-  end
+  resources :prototypes,only:[:index, :show]
+  resources :comments,only: :create
+  
 end
